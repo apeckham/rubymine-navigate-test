6ba964c9d91866b52b20f0a5ff1dadb20ff0b3d3
apeckham <apeckham@gmail.com>
2014-08-15 16:05:00 -0700

    refactor

--- rubymine_navigate_test.rb
+++ rubymine_navigate_test.rb
@@ -1,15 +1,18 @@
 require 'fileutils'
 
-def toggle(file)
+def find_opposite(file)
   if file.include?('spec')
-    file_to_open = file.sub(/_spec/, '').sub(/^spec\//, 'app/')
+    file.sub(/_spec/, '').sub(/^spec\//, 'app/')
   else
-    file_to_open = file.sub(/\.(\w+)$/, '_spec.\1').sub(/^app\//, 'spec/')
+    file.sub(/\.(\w+)$/, '_spec.\1').sub(/^app\//, 'spec/')
   end
+end
 
-  FileUtils.mkdir_p File.dirname(file_to_open)
-  FileUtils.touch file_to_open
-  Kernel.system '/usr/local/bin/mine', file_to_open
+def toggle(file)
+  opposite = find_opposite(file)
+  FileUtils.mkdir_p File.dirname(opposite)
+  FileUtils.touch opposite
+  Kernel.system '/usr/local/bin/mine', opposite
 end
 
 toggle(ARGV[0] || raise) if __FILE__ == $0
