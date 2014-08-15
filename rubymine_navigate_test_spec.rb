af0329d9b0d02feaf6d039d5e2396d008928306b
apeckham <apeckham@gmail.com>
2014-08-15 15:56:20 -0700

    it opens an existing impl

--- rubymine_navigate_test_spec.rb
+++ rubymine_navigate_test_spec.rb
@@ -14,7 +14,7 @@ describe 'toggle' do
     expect(all_files).to be_empty
   end
 
-  it 'opens an existing file' do
+  it 'opens an existing spec' do
     FileUtils.touch 'spec/controller/application_controller_spec.rb'
     stub_const 'ARGV', ['app/controller/application_controller.rb']
     expect(Kernel).to receive(:system).with '/usr/local/bin/mine', '/spec/controller/application_controller_spec.rb'
@@ -22,10 +22,18 @@ describe 'toggle' do
     expect(all_files).to eq ['/spec/controller/application_controller_spec.rb']
   end
 
-  it 'creates and opens a file' do
+  it 'creates and opens a spec' do
     stub_const 'ARGV', ['app/controller/application_controller.rb']
     expect(Kernel).to receive(:system).with '/usr/local/bin/mine', 'spec/controller/application_controller_spec.rb'
     toggle
     expect(all_files).to eq ['/spec/controller/application_controller_spec.rb']
+  end
+
+  it 'opens an existing impl' do
+    FileUtils.touch 'app/controller/application_controller.rb'
+    stub_const 'ARGV', ['spec/controller/application_controller_spec.rb']
+    expect(Kernel).to receive(:system).with '/usr/local/bin/mine', '/app/controller/application_controller.rb'
+    toggle
+    expect(all_files).to eq ['/app/controller/application_controller.rb']
   end
 end
